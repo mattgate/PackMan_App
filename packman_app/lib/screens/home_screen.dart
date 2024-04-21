4480d1007f8373a6895006a6c8ea4fc495480c27
mattgate <mattgatexd@gmail.com>
2024-04-20 21:51:21 -0500

    *Se agrega nueva estructura de app packman en flutter

---
+++ packman_app/lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PackMan'),
+        backgroundColor: Colors.yellow,
+      ),
+      backgroundColor: Colors.yellow,
+    );
+  }
+}
